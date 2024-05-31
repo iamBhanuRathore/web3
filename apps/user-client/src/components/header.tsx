ab5f052f1d8c669d7e148489e76f707ddc2e52d4
bhanuo07 <rathoreb853@gmail.com>
2024-05-31 18:39:43 +0530

    added sign in both frontend and backend

--- apps/user-client/src/components/header.tsx
+++ apps/user-client/src/components/header.tsx
@@ -1,21 +1,14 @@
 // import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./theme-toggler,";
-import {
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useEffect } from "react";
+
+import { WalletMultiButton } from "@solana/wallet-adapter-ant-design";
+// import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 const Header = () => {
-  const {
-    // connected,
-    publicKey,
-    // wallets
-  } = useWallet();
-  // console.log(
-  //   // wallets
-  //   publicKey
-  // );
+  const { publicKey } = useWallet();
+
   useEffect(() => {
     if (!publicKey) return;
   }, [publicKey]);
@@ -25,9 +18,7 @@ const Header = () => {
         <p>Web 3 User </p>
         <div className="flex justify-center items-center gap-x-4">
           <ModeToggle />
-          <WalletMultiButton />
-          {/* {connected ? <WalletDisconnectButton /> : <WalletMultiButton />} */}
-          {/* {!connected ? <WalletDisconnectButton /> : <WalletMultiButton />} */}
+          <WalletMultiButton className="flex items-center justify-center " />
         </div>
       </div>
     </>
@@ -35,89 +26,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   darkMode: ["class"],
-//   content: [
-//     "./pages/**/*.{ts,tsx}",
-//     "./components/**/*.{ts,tsx}",
-//     "./app/**/*.{ts,tsx}",
-//     "./src/**/*.{ts,tsx}",
-//   ],
-//   prefix: "",
-//   theme: {
-//     container: {
-//       center: true,
-//       padding: "2rem",
-//       screens: {
-//         "2xl": "1400px",
-//       },
-//     },
-//     extend: {
-//       colors: {
-//         border: "hsl(var(--border))",
-//         input: "hsl(var(--input))",
-//         ring: "hsl(var(--ring))",
-//         background: "hsl(var(--background))",
-//         foreground: "hsl(var(--foreground))",
-//         primary: {
-//           DEFAULT: "hsl(var(--primary))",
-//           foreground: "hsl(var(--primary-foreground))",
-//         },
-//         secondary: {
-//           DEFAULT: "hsl(var(--secondary))",
-//           foreground: "hsl(var(--secondary-foreground))",
-//         },
-//         destructive: {
-//           DEFAULT: "hsl(var(--destructive))",
-//           foreground: "hsl(var(--destructive-foreground))",
-//         },
-//         muted: {
-//           DEFAULT: "hsl(var(--muted))",
-//           foreground: "hsl(var(--muted-foreground))",
-//         },
-//         accent: {
-//           DEFAULT: "hsl(var(--accent))",
-//           foreground: "hsl(var(--accent-foreground))",
-//         },
-//         popover: {
-//           DEFAULT: "hsl(var(--popover))",
-//           foreground: "hsl(var(--popover-foreground))",
-//         },
-//         card: {
-//           DEFAULT: "hsl(var(--card))",
-//           foreground: "hsl(var(--card-foreground))",
-//         },
-//       },
-//       borderRadius: {
-//         lg: "var(--radius)",
-//         md: "calc(var(--radius) - 2px)",
-//         sm: "calc(var(--radius) - 4px)",
-//       },
-//       keyframes: {
-//         "accordion-down": {
-//           from: { height: "0" },
-//           to: { height: "var(--radix-accordion-content-height)" },
-//         },
-//         "accordion-up": {
-//           from: { height: "var(--radix-accordion-content-height)" },
-//           to: { height: "0" },
-//         },
-//       },
-//       animation: {
-//         "accordion-down": "accordion-down 0.2s ease-out",
-//         "accordion-up": "accordion-up 0.2s ease-out",
-//       },
-//     },
-//   },
-//   plugins: [require("tailwindcss-animate")],
-// };
-
-// // eslint-disable-next-line no-undef
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
--- apps/user-client/src/providers/solana-provider.tsx
+++ apps/user-client/src/providers/solana-provider.tsx
@@ -5,15 +5,12 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 // import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 
-// Default styles that can be overridden by your app
-import("@solana/wallet-adapter-react-ui/styles.css");
+import { WalletModalProvider } from "@solana/wallet-adapter-ant-design";
+import("@solana/wallet-adapter-ant-design/styles.css");
+// import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+// import("@solana/wallet-adapter-react-ui/styles.css");
 
 export const SolanaProvider = ({ children }: { children: ReactNode }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
@@ -36,21 +33,15 @@ export const SolanaProvider = ({ children }: { children: ReactNode }) => {
       //      * instantiate its legacy wallet adapter here. Common legacy adapters can be found
       //      * in the npm package `@solana/wallet-adapter-wallets`.
       //      */
-      //     new UnsafeBurnerWalletAdapter(),
+      // new UnsafeBurnerWalletAdapter(),
     ],
-    //   // eslint-disable-next-line react-hooks/exhaustive-deps
     [network]
   );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          {/* <WalletMultiButton />
-          <WalletDisconnectButton /> */}
-          {children}
-          {/* Your app's components go here, nested within the context providers. */}
-        </WalletModalProvider>
+        <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
